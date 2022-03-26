4121ebe677d668bb34a7e31f63dc8be34dce4f86
0x0015 <64482595+0x0015@users.noreply.github.com>
2022-03-26 00:44:53 -0400

    Adedd more example code

--- main.cpp
+++ main.cpp
@@ -11,15 +11,6 @@ Vector zero(Vector n){
 	return(Vector(n.data.size()));
 }
 
-Vector t2(Vector n){
-	Vector d;
-	d.data = n.data;
-	for(auto& i : d.data){
-		i*=2;
-	}
-	return(d);
-}
-
 Vector ex(Vector n){
 	Vector out;
 	out.data = {n[0]+3*n[1], 0, 2*n[0]-4*n[1]};
@@ -31,12 +22,13 @@ Matrix tran(Matrix m){
 }
 
 int main() {
+	std::cout<<"Mat R^3 Identity transformation:"<<std::endl;
+	Transformation<Real<3>, Real<3>> id_tr(identity);
+	id_tr.getMatRep().print();
+	std::cout<<"Example from class: T:R^2->R^3 T(a1, a2) = (a1+3*a2, 0, 2a1-4a2)"<<std::endl;
 	Transformation<Real<2>, Real<3>> tr(ex);
-	tr.vval.Basis[0].print();
-	Matrix rep = tr.getMatRep();
-	//rep.print();
-	
+	tr.getMatRep().print();
+	std::cout<<"Mat 3x3 transpose:"<<std::endl;
 	Transformation<MatR<3,3>, MatR<3,3>> tr2(tran);
-	Matrix rep2 = tr2.getMatRep();
-	rep2.print();
+	tr2.getMatRep().print();
 }
